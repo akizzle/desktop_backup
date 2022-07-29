a7cc4e32cf221d73efa9ee3adcfe4c4f5da6df16
akizzle <artoaky@gmail.com>
2022-07-29 09:13:43 -0400

    Update configsBackup.ps1

--- configsBackup.ps1
+++ configsBackup.ps1
@@ -26,45 +26,49 @@ $appDirs = @{
     #documents       = "$env:USERPROFILE\Documents"
     #pictures        = "$env:USERPROFILE\Pictures"
     #downloads       = "$env:USERPROFILE\Downloads"
+    #desktop         = "$env:USERPROFILE\Desktop"
 } 
 
 # specify backup dir
 $backupDir = "D:\Config Backups"
+# set backup date for files
 $fileDate = Get-Date -Format "MM.dd.yyyy_HH.mm.ss"
+# set 7zip executable
 $7z = "$env:ProgramFiles\7-zip\7z.exe"
 
 try{
     if($action -eq 'backup'){
         # choco package list backup
-        .export-chocolatey.ps1 > "$backupDir\packages.config"
+        .export-chocolatey.ps1 > "$backupDir\packages$fileDate.config"
         if(Test-Path -path $7z){
             $appDirs.GetEnumerator() | ForEach-Object{
-                if($action -eq 'backup'){
-                    if(Test-Path $_.Value){
-                        if($_.Key -eq "lgHub"){
-                            #kills lghub processes due to file in use
-                            # Get-Process | Where-Object {$_.Name -like 'lghub*'} | Stop-Process -Force
-                        }
-                    #archives targetted directories
-                    & $7z a "$backupDir\$($_.Key)_$fileDate.zip" $_.Value
-                   }
+                # deletes any backup zip older than 14 days.
+                Get-ChildItem -path "$backupDir\$($_.Key)*" | Sort-object -Property LastWriteTime | Where-Object {LastWriteTime -gt (Get-Date).AddDays(-14)} | Remove-Item -Force
+                if(Test-Path $_.Value){
+                    if($_.Key -eq "lgHub"){
+                        # kills lghub processes due to file in use
+                        # Get-Process | Where-Object {$_.Name -like 'lghub*'} | Stop-Process -Force
+                    }
+                # archives targetted directories
+                & $7z a "$backupDir\$($_.Key)_$fileDate.zip" $_.Value
                 }
             }
         }
     }elseif($action -eq 'restore'){
-        #install choco
+        # install choco
         Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-        #install 7zip to perform extraction ahead.
+        # install 7zip to perform extraction ahead.
         choco install 7zip
-        #install packages 
-        choco install packages.config -y
+        # retrieves latest package backup
+        $chocoLatest = Get-ChildItem -path "$backupDir\packages*" | Sort-Object -Property LastWriteTime | Select-Object -last 1
+        # install packages
+        choco install $chocoLatest -y
         $appDirs.GetEnumerator() | ForEach-Object{
             #place backup items 
-            $restoreZip = Get-ChildItem -path "$backupDir\$($_.Key)*" | Sort-Object LastWriteTime | Select-Object -last 1
+            $restoreZip = Get-ChildItem -path "$backupDir\$($_.Key)*" | Sort-Object -Property LastWriteTime | Select-Object -last 1
             #extracts zip in original locations
             & $7z e $restorezip -o$_.Value
         }
-
     }else{
         Write-Output 'No action taken.'
     }

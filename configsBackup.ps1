3f450dbb55b1b7acdccc3b98712bfd1a51eff4fb
akizzle <artoaky@gmail.com>
2022-07-29 09:24:57 -0400

    Update configsBackup.ps1

--- configsBackup.ps1
+++ configsBackup.ps1
@@ -23,10 +23,10 @@ $appDirs = @{
     wowWTF          = "D:\Program Files (x86)\World of Warcraft\_retail_\WTF"
     iCUE            = "$env:USERPROFILE\AppData\Roaming\Corsair\CUE Backup"
     lgHub           = "$env:USERPROFILE\Appdata\Local\LGHUB"
-    #documents       = "$env:USERPROFILE\Documents"
-    #pictures        = "$env:USERPROFILE\Pictures"
-    #downloads       = "$env:USERPROFILE\Downloads"
-    #desktop         = "$env:USERPROFILE\Desktop"
+    documents       = "$env:USERPROFILE\Documents"
+    pictures        = "$env:USERPROFILE\Pictures"
+    downloads       = "$env:USERPROFILE\Downloads"
+    desktop         = "$env:USERPROFILE\Desktop"
 } 
 
 # specify backup dir
